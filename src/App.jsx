cb77b9f1915bbf1b22addb5f7b4eb8dc69dc3ab0
romanazaruk <canspro1105@gmail.com>
2020-04-15 19:21:05 +0300

    Added progress

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="intro">
+        <h1 className="header">Your application so on the way.</h1>
+        <h3 className="description">We shipped your package</h3>
+      </div>
+      <div className="progress">
+        <div className="dot_info">
+          <div className="dot"></div>
+          <div className="info_first">Ordered</div>
+          <div className="info_first_date">May 10</div>
+          </div>
+        <div className="dotmini"></div>
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>        
+        <div className="dot_info">
+          <div className="dot"></div>
+          <div className="info_second">Estimated May 11</div>
+          <div className="info_address">66 Perry Street</div>
+          <div className="info_state">MWest village, NY 10014</div>
+          </div>
+        <div className="dotmini"></div>
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div> 
+        <div className="dot_info">
+          <div className="dot"></div>
+          <div className="info_third">Estimated May 12</div>
+          <div className="info_office">Health department Office</div>
+          </div>
+        <div className="dotmini"></div>
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div>  
+        <div className="dotmini"></div> 
+        <div className="dot_info">
+          <div className="dot"></div>
+          <div className="info_fourth">Estimated May 22</div>
+          <div className="info_address">66 Perry Street</div>
+          <div className="info_state">West village,NY 10014</div>
+          </div>
+          
+      </div>
+    </div>
+  );
+}
+
+export default App;
