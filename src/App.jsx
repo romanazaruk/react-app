10b90b4b23b36fac536862bf8f5c805fc886adf5
romanazaruk <canspro1105@gmail.com>
2020-04-21 23:30:05 +0300

    added props to functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import Intro from "./components/Intro";
-import Step from "./components/Step"
-import DottedLine from "./components/DottedLine"
-import Progress from "./components/Progress"
+import Step from "./components/Step";
+import DottedLine from "./components/DottedLine";
+import Progress from "./components/Progress";
 
 function App() {
   return (
     <div className="App">
-      <Intro/>
+      <Intro />
       <div className="progress">
-      <Step name="Ordered" date="May 10"/>
-      <DottedLine/>
-      <Step name="Estimated May 11" address="66 Perry Street" state="West village,NY 10014" />
-      <DottedLine/>
-      <Step name="Estimated May 12" office="Health department Office"/>
-      <DottedLine/>
-      <Step name="Estimated May 22" address="66 Perry Street" state="West village,NY 10014" />
+        <Step name="Ordered" description="May 10" />
+        <DottedLine />
+        <Step
+          name="Estimated May 11"
+          description={<>66 Perry Street<br/> West village,NY 10014</>}
+        />
+        <DottedLine />
+        <Step name="Estimated May 12" description="Health department Office" />
+        <DottedLine />
+        <Step
+          name="Estimated May 22"
+          description="66 Perry Street"
+          description="West village,NY 10014"
+        />
       </div>
     </div>
   );
--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -2,28 +2,12 @@ import React from "react";
 
 export default function Step(props) {
   return (
-    <div>
       <div className="dot_container">
         <div className="dot"></div>
         <div className="text_description">
           <h3>{props.name}</h3>
-          <h5>{props.date}</h5>
-        </div>
-        <div className="text_description">
-          <h3>{props.name}</h3>
-          <h5>{props.address}</h5>
-          <h5>{props.state}</h5>
-        </div>
-        <div className="text_description">
-          <h3>{props.name}</h3>
-          <h5>{props.office}</h5>
-        </div>
-        <div className="text_description">
-          <h3>{props.name} </h3>
-          <h5>{props.address}</h5>
-          <h5>{props.state}</h5>
+          <h5>{props.description}</h5>
         </div>
       </div>
-    </div>
   );
 }
